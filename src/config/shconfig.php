a33986746f8420325ac2b971bb80eba420163d4c
Hosea Kibet <iankibet@Hoseas-Mac-mini.local>
2026-03-31 06:59:19 +0300

    Add configurable route overrides for sh-departments module permissions routes

--- src/config/shconfig.php
+++ src/config/shconfig.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'api_middleware'=>env('SH_API_MIDDLEWARE','auth:sanctum')
+    'api_middleware'=>env('SH_API_MIDDLEWARE','auth:sanctum'),
+    'route_overrides' => [
+        'sh_departments' => [
+            'department' => [
+                'list_all_modules' => null,
+                'get_module_permissions' => null,
+            ],
+        ],
+    ],
 ];
